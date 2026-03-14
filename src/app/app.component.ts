dabfe970e0feda5b90c98ed01fb2b850f6a67c29
Magfirat80 <magfirat80@mail.ru>
2026-03-15 00:59:28 +0400

    working with routing and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,183 +2,35 @@ import { Component, inject } from '@angular/core';
 import './training'
 import { Color } from '../enums/Color';
 import './collection'
-import { IAdvantage } from '../interfaces/IAdvantage';
 import { FormsModule } from '@angular/forms';
-import { IParticipantsCount } from '../interfaces/IParticipantsCount';
-import { ITourLocation } from '../interfaces/ITourLocation';
-import { IDestinationMap } from '../interfaces/IDestinationMap';
-import { IArticle } from '../interfaces/IArticle';
 import { MessageService } from './message.service';
-import { Message } from "../enums/Message";
-import { NgTemplateOutlet, NgComponentOutlet } from '@angular/common';
 import { LocalStorageService } from './local-storage.service';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../footer/footer.component";
+import { HeaderComponent } from "../header/header.component";
+import { MessageComponent } from "../message/message.component";
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, NgTemplateOutlet],
+  imports: [FormsModule, RouterOutlet, FooterComponent, HeaderComponent, MessageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [MessageService, LocalStorageService]
 })
 export class AppComponent {
   
-  messageService: MessageService = inject(MessageService);
   localStorageService: LocalStorageService = inject(LocalStorageService);
 
-  message: typeof Message = Message;
-  companyName: string = 'румтибет';
-  selectedTour: string = '';
-  selectedGroup: string = '';
-  selectedDate!: string;
-  selectedAdvantageId: number = 2;
-  currentDateAndTime: string = new Date().toLocaleString();
-  counter: number = 0;
-  currentWidget:  'counter' | 'date' = 'counter';
-  liveInput!: string;
   isLoading: boolean = true;
-
-  locations: ITourLocation[] = [
-    {
-      id: 1,
-      tourLocation: 'Восхождение на Эльбрус'
-    },
-    {
-      id: 2,
-      tourLocation: 'Кольцо Эльбруса'
-    },
-    {
-      id: 3,
-      tourLocation: 'Поход к Белухе'
-    },
-    {
-      id: 4,
-      tourLocation: 'Шавлинские озёра'
-    },
-    {
-      id: 5,
-      tourLocation: 'Уральский Барс'
-    },
-    {
-      id: 6,
-      tourLocation: 'Треккинг в Хибинах'
-    },
-    {
-      id: 7,
-      tourLocation: 'Долина вулканов'
-    },
-  ];
-
-  groups: IParticipantsCount[] = [
-    {
-      id: 1,
-      participantsCount: '1-4 человека'
-    },
-    {
-      id: 2,
-      participantsCount: '5-8 человек'
-    },
-    {
-      id: 3,
-      participantsCount: '9-12 человек'
-    },
-    {
-      id: 4,
-      participantsCount: '13-18 человек'
-    },
-    {
-      id: 5,
-      participantsCount: '19 человек и более'
-    },
-  ];
-
-  advantages: IAdvantage[] = [
-    {
-      id: 1,
-      icon: 'gid-icon',
-      title: 'Опытный гид',
-      description: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.'
-    },
-    {
-      id: 2,
-      icon: 'hike-icon',
-      title: 'Безопасный поход',
-      description: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.'
-    },
-    {
-      id: 3,
-      icon: 'prices-icon',
-      title: 'Лояльные цены',
-      description: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.'
-    },
-  ];
-
-  maps: IDestinationMap[] = [
-    {
-      estimation: '4.9',
-      name: 'Озеро возле гор',
-      description: 'романтическое приключение',
-      cost: 480,
-      poster: 'destination-lake'
-    },
-    {
-      estimation: '4.5',
-      name: 'Ночь в горах',
-      description: 'в компании друзей',
-      cost: 500,
-      poster: 'destination-night'
-    },
-    {
-      estimation: '5.0',
-      name: 'Спорт в горах',
-      description: 'для тех, кто заботится о себе',
-      cost: 230,
-      poster: 'destination-sport'
-    },
- ];
-
- articles: IArticle[] = [
-  {
-    id: 1,
-    articleIllustration: 'article-Italy',
-    title: 'Красивая Италия, какая она в реальности?',
-    introduction: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
-    date: '01/04/2023'
-  },
-  {
-    id: 2,
-    articleIllustration: 'article-flight',
-    title: 'Долой сомнения! Весь мир открыт для вас!',
-    introduction: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации ... независимые способы реализации соответствующих...',
-    date: '01/04/2023'
-  },
-  {
-    id: 3,
-    articleIllustration: 'article-travel-alone',
-    title: 'Как подготовиться к путешествию в одиночку? ',
-    introduction: 'Для современного мира базовый вектор развития предполагает.',
-    date: '01/04/2023'
-  },
-  {
-    id: 4,
-    articleIllustration: 'article-India',
-    title: 'Индия ... летим?',
-    introduction: 'Для современного мира базовый.',
-    date: '01/04/2023'
-  },
- ];
 
   constructor() {
     this.isMainColor(Color.YELLOW);
     this.saveLastVisitDate();
     this.saveEntriesCount();
-
-    setInterval(() => {
-      this.currentDateAndTime = new Date().toLocaleString();
-    }, 1000);
     
     setTimeout(() => {
       this.isLoading = false;
-    }, 2000);
+    }, 400);
   }
 
   private isMainColor(color: Color): boolean {
@@ -194,21 +46,5 @@ export class AppComponent {
     let entriesCount: number = Number(this.localStorageService.getValue('entries-count')) || 0;
     entriesCount++;
     this.localStorageService.setValue('entries-count', String(entriesCount));
-  }
-
-  selectAdvantage(advantageId: number): void {
-    this.selectedAdvantageId = advantageId;
-  }
-
-  increaseCount(): void {
-    this.counter++;
-  }
-  
-  reduceCount(): void {
-    this.counter--;
-  }
-
-  switchWidget(widget: 'counter' | 'date'): void {
-    this.currentWidget = widget;
   }
 }
---
+++ src/home-page/home-page.component.ts
@@ -0,0 +1,162 @@
+import { Component, inject } from '@angular/core';
+import { ITourLocation } from '../interfaces/ITourLocation';
+import { IAdvantage } from '../interfaces/IAdvantage';
+import { IDestinationMap } from '../interfaces/IDestinationMap';
+import { IParticipantsCount } from '../interfaces/IParticipantsCount';
+import { IArticle } from '../interfaces/IArticle';
+import { FormsModule } from '@angular/forms';
+import { Message } from '../enums/Message';
+import { MessageService } from '../app/message.service';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [FormsModule],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss',
+})
+export class HomePageComponent {
+
+  messageService: MessageService = inject(MessageService);
+  
+  selectedTour: string = '';
+  selectedGroup: string = '';
+  selectedDate!: string;
+  liveInput!: string;
+  selectedAdvantageId: number = 2;
+  message: typeof Message = Message;
+
+  locations: ITourLocation[] = [
+    {
+      id: 1,
+      tourLocation: 'Восхождение на Эльбрус'
+    },
+    {
+      id: 2,
+      tourLocation: 'Кольцо Эльбруса'
+    },
+    {
+      id: 3,
+      tourLocation: 'Поход к Белухе'
+    },
+    {
+      id: 4,
+      tourLocation: 'Шавлинские озёра'
+    },
+    {
+      id: 5,
+      tourLocation: 'Уральский Барс'
+    },
+    {
+      id: 6,
+      tourLocation: 'Треккинг в Хибинах'
+    },
+    {
+      id: 7,
+      tourLocation: 'Долина вулканов'
+    },
+  ];
+
+  groups: IParticipantsCount[] = [
+    {
+      id: 1,
+      participantsCount: '1-4 человека'
+    },
+    {
+      id: 2,
+      participantsCount: '5-8 человек'
+    },
+    {
+      id: 3,
+      participantsCount: '9-12 человек'
+    },
+    {
+      id: 4,
+      participantsCount: '13-18 человек'
+    },
+    {
+      id: 5,
+      participantsCount: '19 человек и более'
+    },
+  ];
+
+  advantages: IAdvantage[] = [
+    {
+      id: 1,
+      icon: 'gid-icon',
+      title: 'Опытный гид',
+      description: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.'
+    },
+    {
+      id: 2,
+      icon: 'hike-icon',
+      title: 'Безопасный поход',
+      description: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.'
+    },
+    {
+      id: 3,
+      icon: 'prices-icon',
+      title: 'Лояльные цены',
+      description: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.'
+    },
+  ];
+
+  maps: IDestinationMap[] = [
+    {
+      estimation: '4.9',
+      name: 'Озеро возле гор',
+      description: 'романтическое приключение',
+      cost: 480,
+      poster: 'destination-lake'
+    },
+    {
+      estimation: '4.5',
+      name: 'Ночь в горах',
+      description: 'в компании друзей',
+      cost: 500,
+      poster: 'destination-night'
+    },
+    {
+      estimation: '5.0',
+      name: 'Спорт в горах',
+      description: 'для тех, кто заботится о себе',
+      cost: 230,
+      poster: 'destination-sport'
+    },
+  ];
+
+  articles: IArticle[] = [
+    {
+      id: 1,
+      articleIllustration: 'article-Italy',
+      title: 'Красивая Италия, какая она в реальности?',
+      introduction: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
+      date: '01/04/2023'
+    },
+    {
+      id: 2,
+      articleIllustration: 'article-flight',
+      title: 'Долой сомнения! Весь мир открыт для вас!',
+      introduction: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации ... независимые способы реализации соответствующих...',
+      date: '01/04/2023'
+    },
+    {
+      id: 3,
+      articleIllustration: 'article-travel-alone',
+      title: 'Как подготовиться к путешествию в одиночку? ',
+      introduction: 'Для современного мира базовый вектор развития предполагает.',
+      date: '01/04/2023'
+    },
+    {
+      id: 4,
+      articleIllustration: 'article-India',
+      title: 'Индия ... летим?',
+      introduction: 'Для современного мира базовый.',
+      date: '01/04/2023'
+    },
+  ];
+
+  selectAdvantage(advantageId: number): void {
+    this.selectedAdvantageId = advantageId;
+  }
+
+}
