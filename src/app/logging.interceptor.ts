ecb311e6a561d6a12edbf25d7402f1ef8eb39d4f
Magfirat80 <magfirat80@mail.ru>
2026-06-01 22:40:53 +0400

    errors correction plus

--- src/app/logging.interceptor.ts
+++ src/app/logging.interceptor.ts
@@ -1,22 +1,22 @@
-import { HttpEvent, HttpRequest, HttpHandlerFn, HttpEventType, HttpResponse } from '@angular/common/http';
-import { tap, Observable } from 'rxjs';
+import { HttpEvent, HttpRequest, HttpHandlerFn, HttpResponse } from '@angular/common/http';
+import { tap, Observable, catchError, throwError } from 'rxjs';
 
 export function loggingInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
   
   const startTime: number = Date.now();
+  const requestInfo: string = `${ req.method } ${ req.urlWithParams }`;
 
   return next(req).pipe(
-    tap({
-      next: (event) => {
-        if (event instanceof HttpResponse) {
-          const duration: number = Math.round(Date.now() - startTime);
-          console.log('[HTTP Done]', req.method, req.urlWithParams, `status: ${ event.status }`, `duration: ${ duration } ms`);
-        }
-      },
-      error: (err) => {
+    tap(event => {
+      if (event instanceof HttpResponse) {
         const duration: number = Math.round(Date.now() - startTime);
-        console.log('[HTTP Error]', req.method, req.urlWithParams, `duration: ${ duration } ms`, err);
+        console.log('[HTTP Done]', requestInfo, `status: ${ event.status }`, `duration: ${ duration } ms`);
       }
+    }),
+    catchError(err => {
+      const duration: number = Math.round(Date.now() - startTime);
+      console.log('[HTTP Error]', requestInfo, `duration: ${ duration } ms`, err);
+      return throwError(() => err);
     })
   );
 };
